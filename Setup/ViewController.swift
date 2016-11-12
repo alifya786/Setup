03c7bd674d44230af641256ede04ab018f84e118
alifya <alifyamo@hotmail.com>
2016-11-12 06:58:12 -0800

    Initial Commit

---
+++ Setup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Setup
+//
+//  Created by Alifya Mohamed. on 2016-11-12.
+//  Copyright © 2016 Setup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
